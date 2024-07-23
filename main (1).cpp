5d285a4d6072d240a4bb9d6770246619dbf930ad
Fraknoomuh <504jacberd@nhusd.k12.ca.us>
2024-07-23 14:42:37 -0700

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+/* For more information of "for loops " look back at the activities 4.2 and 4.4 */
+
+int main() {
+
+  /*
+  for(int a = 1; a <= 5; a++) { // The number of text there will be
+    for(int b = 1; b <= 5 - a; b++) { // The spaces, it decreases 
+      cout << " ";
+    }
+    for(int c = 1; c <= a; c++) { // The left over space for the stars to be print
+      cout << "*";
+    }
+    cout << endl; // Causes a line break after 
+  }
+  */
+  
+  // Calculate numbers from 1 - 100
+  /*
+  int sum = 0;
+  for(int i = 1; i <= 100; i++) {
+    sum += i;
+  }
+  cout << sum << endl;
+  */
+
+  // Display your name 18 times
+  
+  for(int a = 1; a <= 18; a++) {
+    cout << a << ": Jacob\n";
+  }
+  
+  // Print beckwords 10 - 1
+  
+  for(int b = 10; b > 0; b--) {
+    cout << b << " ";
+  }
+  cout << "\n";
+  //Print the multiplication table of 5 up to 10
+  
+  for(int c = 5; c <= 50; c += 5) {
+    cout << c << " ";
+  }
+  a *=
+
+
+  
+  return 0;
+  
+} // End of main
